f0225b9962a0c2b64ea5327b7e72ec3d286576a9
Salitre97 <salitrecristian@gmail.com>
2024-04-20 18:18:30 -0400

    adde cnn_encoderdecoder.py just a simple model

---
+++ src/training/cnn_encoderdecoder.py
@@ -0,0 +1,219 @@
+# In[0]
+
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import torch 
+import torch.nn as nn
+import os
+from PIL import Image, ImageDraw
+import numpy as np
+from matplotlib.patches import Polygon
+import torch.optim as optim
+import torchvision.transforms as transforms
+import albumentations as A
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+import cv2
+import random 
+import matplotlib.patches as patches
+import skimage.draw 
+import tifffile 
+import shutil
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
+# cristian path 
+base_dir = '/home/csalitre/school/ecgr-5106/final-project/' 
+
+train_path = base_dir + 'semantic-segmentation/train2/' 
+valid_path = base_dir + 'semantic-segmentation/valid2/'
+test_path = base_dir + 'semantic-segmentation/test2/'
+
+image_transform = transforms.Compose([
+    transforms.Resize(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485], std=[0.229]),  # Assuming grayscale images
+    transforms.Lambda(lambda x: x.clamp(0, 1))
+])
+
+class BrainDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.image_folder = os.path.join(root_dir, "images")
+        self.mask_folder = os.path.join(root_dir, "masks")
+        self.image_files = sorted(os.listdir(self.image_folder))
+        self.mask_files = sorted(os.listdir(self.mask_folder))
+        self.transform = transform
+
+        # Print lengths of image_files and mask_files for debugging
+        print(f"Length of image files: {len(self.image_files)}")
+        print(f"Length of mask files: {len(self.mask_files)}")
+
+     # Check consistency of image and mask files
+        if len(self.image_files) != len(self.mask_files):
+            mismatched_file_index = len(self.image_files) if len(self.image_files) < len(self.mask_files) else len(self.mask_files)
+            print(f"Mismatched files: {self.image_files[mismatched_file_index]}")
+            raise ValueError("Number of images and masks do not match.")
+    
+    
+    def __len__(self):
+        return len(self.image_files)
+    
+    def __getitem__(self,idx):
+        img_name = self.image_files[idx]
+        img_path = os.path.join(self.image_folder, img_name)
+        image_gray = Image.open(img_path).convert("L")  # Convert to grayscale
+        mask_name = self.mask_files[idx]
+        mask_path = os.path.join(self.mask_folder, mask_name)
+        mask = Image.open(mask_path).convert("L")
+        
+        if self.transform:
+            # Apply transformations
+            image_gray = self.transform(image_gray)
+            mask = self.transform(mask)
+        return image_gray, mask
+
+
+
+train_dataset = BrainDataset(train_path, transform=image_transform)
+valid_dataset = BrainDataset(valid_path, transform=image_transform)
+test_dataset = BrainDataset(test_path, transform=image_transform)
+
+# Create data loaders
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+valid_loader = DataLoader(valid_dataset, batch_size=16, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
+
+# Define cnn encoder-decoder model
+class CNN_EncoderDecoder(nn.Module):
+    def __init__(self,in_chans=1, out_chans=64, sampling_factor=2):
+        super(CNN_EncoderDecoder, self).__init__()
+        # Define encoder layers
+        self.encoder = nn.Sequential(
+            nn.Conv2d(in_chans, out_chans, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=sampling_factor),
+            nn.Conv2d(out_chans, out_chans * 2, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=sampling_factor))
+        
+        # Define decoder layers
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(out_chans * 2 , out_chans, kernel_size=2, stride=2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(out_chans, in_chans, kernel_size=2, stride=2),
+            nn.Sigmoid()
+        )
+        
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+    
+
+def train(model, train_loader, valid_loader, num_epochs, learning_rate):
+    # Define loss function
+    criterion = nn.BCELoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    
+    # Move model to device
+    model.to(device)
+     
+    train_loss_history = []
+    valid_loss_history = []
+
+
+    # Training loop
+    for epoch in range(num_epochs):
+        model.train()
+        train_loss = 0.0
+        correct_train = 0
+        total_train =0
+        for images, masks in train_loader:
+            images, masks = images.to(device), masks.to(device)
+
+            optimizer.zero_grad() # zero out the gradients
+            outputs = model(images)
+            loss = criterion(outputs, masks)
+
+            loss.backward() # back propagate the loss
+            optimizer.step()    # update the weights
+            train_loss += loss.item() # add the loss to the train loss
+
+            predicted = outputs > 0.5   # threshold the outputs to get the predicted masks
+            correct_train += (predicted == masks).sum().item()
+            total_train += masks.numel()
+
+        train_accuracy = correct_train / total_train
+
+        # Validation loop
+        model.eval()
+        valid_loss = 0.0
+        correct_valid = 0
+        total_valid = 0
+        with torch.no_grad():
+            for images, masks in valid_loader:
+                images, masks = images.to(device), masks.to(device)
+
+                outputs = model(images)
+                loss = criterion(outputs, masks)
+                valid_loss += loss.item()
+
+                predicted = outputs > 0.5
+                correct_valid += (predicted == masks).sum().item()
+                total_valid += masks.numel()
+
+        valid_accuracy = correct_valid / total_valid
+
+        print(f"Epoch {epoch+1}/{num_epochs}: "
+              f"Train loss: {train_loss/len(train_loader):.4f}, "
+               f"Train accuracy: {train_accuracy:.4f}, "
+               f"Validation loss: {valid_loss/len(valid_loader):.4f}, "
+               f"Validation accuracy: {valid_accuracy:.4f}")
+        
+        # Save the loss values for plotting
+        train_loss_history.append(train_loss/len(train_loader))
+        valid_loss_history.append(valid_loss/len(valid_loader))
+
+    return model, train_loss_history, valid_loss_history
+
+
+model = CNN_EncoderDecoder()
+model, train_loss_history, valid_loss_history = train(model, train_loader, valid_loader, num_epochs=2, learning_rate=0.001)
+
+
+torch.save(model.state_dict(), base_dir + 'cnn_encoderdecoder.pth')
+model.load_state_dict(torch.load(base_dir + 'cnn_encoderdecoder.pth'))
+
+image_path = os.path.join(base_dir, "semantic-segmentation/test2/images/27_jpg.rf.b2a2b9811786cc32a23c46c560f04d07.jpg")
+image = Image.open(image_path).convert("L")
+input_image = image_transform(image).unsqueeze(0).to(device)  
+
+model = CNN_EncoderDecoder().to(device)
+
+model.eval()
+
+with torch.no_grad():
+    output = model(input_image)
+output_np = output.squeeze(0).cpu().numpy()
+
+# In[1]
+import matplotlib.pyplot as plt
+# Plot the input image
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.imshow(image, cmap='gray')
+plt.title('Input Image')
+plt.axis('off')
+
+# Plot the segmentation
+plt.subplot(1, 2, 2)
+plt.imshow(output_np[0], cmap='gray')
+plt.title('Segmentation Mask')
+plt.axis('off')
+
+plt.show()
+# %%
